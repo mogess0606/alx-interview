342d5fc2d6d87e40144897314b71185413472049
Mogess <88327120+mogess0606@users.noreply.github.com>
2022-11-21 07:42:43 +0300

    alx interview  utf validating

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
-""" Represents a valid UTF-8 encoding. """
+"""
+Method that determines if a given data set represents a valid UTF-8 encoding.
+* Return: True if data is a valid UTF-8 encoding, else return False
+* A character in UTF-8 can be 1 to 4 bytes long
+* The data set can contain multiple characters
+* The data will be represented by a list of integers
+* Each integer represents 1 byte of data, therefore you only need
+* to handle the 8 least significant bits of each integer
+"""
+
 
 def validUTF8(data):
-    """
-    Encode UTF-8
-    """
-
-    n_bytes = 0
-
-    for num in data:
-        bin_rep = format(num, '#010b')[-8:]
-        if n_bytes == 0:
-            for bit in bin_rep:
-                if bit == '0':
-                    break
-                n_bytes += 1
-            if n_bytes == 0:
+    """ UTF-8 Validation """
+    bytesLong = 0
+    binaryMoveLeft7 = 1 << 7
+    binaryMoveLeft6 = 1 << 6
+    for byte in data:
+        binaryMove = 1 << 7
+        if bytesLong == 0:
+            while byte & binaryMove:
+                bytesLong += 1
+                binaryMove = binaryMove >> 1
+            if bytesLong == 0:
                 continue
-            if n_bytes == 1 or n_bytes > 4:
+            if bytesLong == 1 or bytesLong > 4:
                 return False
         else:
-            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+            if not (byte & binaryMoveLeft7 and not (byte & binaryMoveLeft6)):
                 return False
-        n_bytes -= 1
-
-    return n_bytes == 0
+        bytesLong -= 1
+    if bytesLong == 0:
+        return True
+    return False
